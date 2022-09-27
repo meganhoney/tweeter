fddf8b641e274edcff20472cb5a71627acb090fc
Megan Honey <mjthoney@gmail.com>
2022-09-27 21:54:20 +0000

    Add loadTweets function with Ajax GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,13 +11,23 @@ $(document).ready(function() {
 
     const $data = $("#tweet-text");
     $data.serialize();
-    $.ajax({
-      type: "POST",
-      url: "/tweets/",
+    $.ajax("/tweets/", {
+      method: "POST",
       data: $data
     })
     console.log($data);
   });
+
+  const loadTweets = function() {
+    $.ajax("/tweets/", {
+      method: "GET"
+    })
+    .then((response) => {
+      renderTweets(response);
+    })
+  }
+
+  loadTweets();
 
   const createTweetElement = function(tweet) {
     // html markup for tweet
@@ -58,31 +68,7 @@ $(document).ready(function() {
 
 
   // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
-renderTweets(data);
+//renderTweets(data);
 
 });
